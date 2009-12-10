334ccd2ed90a628ea3d56314bf07e23c54aa1279
Travis J Parker <travis.parker@gmail.com>
2009-12-09 22:27:45 -0800

    we have a ways to go to be ready for primetime

--- tests.py
+++ tests.py
@@ -31,11 +31,34 @@ class BasicJSONDecodeTests(unittest.TestCase):
     def test_Null(self):
         self.assertDecodesTo('null', None)
 
+    def test_Integer(self):
+        self.assertDecodesTo("1234", 1234)
+
+    def test_Float(self):
+        self.assertDecodesTo("53.2", 53.2)
+
     def test_List(self):
         self.assertDecodesTo('[1,2]', [1, 2])
 
     def test_ListOfFloats(self):
         self.assertDecodesTo('[3.14, 2.718]', [3.14, 2.718])
+
+    def test_ListOfBools(self):
+        self.assertDecodesTo('[true, true, false, true, false, false]',
+                [True, True, False, True, False, False])
+
+    def test_ListOfStrings(self):
+        self.assertDecodesTo('["a", "foo", "bar", "blah"]',
+                ['a', 'foo', 'bar', 'blah'])
+
+    def test_ListOfNulls(self):
+        self.assertDecodesTo('[null, null, null]', [None, None, None])
+
+    def test_List_WhitespaceIgnored(self):
+        self.assertEquals(self.decode("[1,2,3,4,5]"), self.decode("[1, 2, 3, 4, 5]"))
+
+    def test_HeterogeneousList(self):
+        self.assertDecodesTo('[1, 1.3, "foobar", null, true]', [1, 1.3, "foobar", None, True])
 
     def test_Dict(self):
         self.assertDecodesTo('{"key" : "pair"}', {'key' : 'pair'})
@@ -54,16 +77,6 @@ class BasicJSONDecodeTests(unittest.TestCase):
         self.assertDecodesTo('''
             {"key" : {"subkey" : [1, 2, 3]}}''',
                 {'key' : {'subkey' : [1,2,3]}})
-
-    def test_Nonsense(self):
-        self.assertRaises(ValueError, self.decode, "asdjklfakl2")
-
-    def test_BadNumber(self):
-        """bad code that begins numerically
-
-        http://neil.fraser.name/news/2009/12/08/
-        """
-        self.assertRaises(ValueError, self.decode, "123xxx")
 
 class BasicJSONEncodeTests(unittest.TestCase):
     def encode(self, value):
@@ -90,40 +103,65 @@ class BasicJSONEncodeTests(unittest.TestCase):
         self.assertEncodesTo({'key' : 'value'}, '{"key":"value"}')
 
     # Python 3 version
-    def python3_test_UnicodeDict(self):
-            self.assertEncodesTo({'foō' : 'bār'}, '{"foō":"bār"}')
+    def python3_UnicodeDict(self):
+        self.assertEncodesTo({'foō' : 'bār'}, '{"fo\\u014d": "b\\u0101r"}')
 
     # Python 2 version
-    def python2_test_UnicodeDict(self):
-            self.assertEncodesTo({unicode('fo\\u014d') : unicode('b\\u0101r')}, '{"fo\\\\u014d":"b\\\\u0101r"}')
+    def python2_UnicodeDict(self):
+        self.assertEncodesTo(
+                {'fo\xc5\x8d'.decode('utf-8'): 'b\xc4\x81r'.decode('utf-8')},
+                r'{"fo\u014d": r"b\u0101r"}')
 
     def test_NestedDictAndList(self):
         self.assertEncodesTo({'key' : {'subkey' : [1,2,3]}},
             '{"key":{"subkey":[1,2,3]}}')
 
 if sys.version_info[0] > 2:
-    BasicJSONEncodeTests.test_UnicodeDict = BasicJSONEncodeTests.python3_test_UnicodeDict
+    BasicJSONEncodeTests.test_UnicodeDict = BasicJSONEncodeTests.python3_UnicodeDict
+    del BasicJSONEncodeTests.python3_UnicodeDict
+    BasicJSONEncodeTests.test_UnicodeDict.__name__ = "test_UnicodeDict"
 else:
-    BasicJSONEncodeTests.test_UnicodeDict = BasicJSONEncodeTests.python2_test_UnicodeDict
+    BasicJSONEncodeTests.test_UnicodeDict = BasicJSONEncodeTests.python2_UnicodeDict
+    del BasicJSONEncodeTests.python2_UnicodeDict
+    BasicJSONEncodeTests.test_UnicodeDict.im_func.__name__ = "test_UnicodeDict"
 
 
 class LoadsTest(BasicJSONDecodeTests):
     def decode(self, json):
         return yajl.loads(json)
 
+
 class DumpsTest(BasicJSONEncodeTests):
     def encode(self, value):
         return yajl.dumps(value)
 
-class ErrorCasesTests(unittest.TestCase):
-    def setUp(self):
-        self.d = yajl.Decoder()
+
+class DecodeErrorTests(unittest.TestCase):
+    def decode(self, text):
+        return yajl.Decoder().decode(text)
+
+    def assertDecodeFails(self, exc_type, text):
+        self.assertRaises(exc_type, self.decode, text)
 
     def test_EmptyString(self):
-        self.failUnlessRaises(ValueError, self.d.decode, '')
+        self.assertDecodeFails(ValueError, '')
 
     def test_None(self):
-        self.failUnlessRaises(ValueError, self.d.decode, None)
+        self.assertDecodeFails(ValueError, None)
+
+    def test_Nonsense(self):
+        self.assertDecodeFails(ValueError, "asdjklfakl2")
+
+    def test_BadInt(self):
+        self.assertDecodeFails(ValueError, "123xxx")
+
+    def test_BadFloat(self):
+        self.assertDecodeFails(ValueError, "4.5xxx")
+
+
+class LoadsErrorTests(DecodeErrorTests):
+    def decode(self, text):
+        return yajl.loads(text)
 
 
 class StreamBlockingDecodingTests(unittest.TestCase):
