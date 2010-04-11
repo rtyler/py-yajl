d8fa6202ccab747d969e85a9da3ad999dcb1890c
R. Tyler Ballance <tyler@monkeypox.org>
2010-04-10 17:55:12 -0700

    Add `yajl.monkeypatch` to smuggle yajl into sys.modules['json']
    http://github.com/rtyler/py-yajl/issues#issue/13
    
    Change-Id: I46924a0a2379c37a2cfc07def071ce59a22761d0

--- tests/unit.py
+++ tests/unit.py
@@ -294,6 +294,14 @@ class IssueTwelveTest(unittest.TestCase):
             from tests import python2
             self.assertEquals(yajl.dumps(python2.IssueTwelveTest_dict), '{"a":"b","c":"d"}')
 
+class IssueThirteenTest(unittest.TestCase):
+    def runTest(self):
+        import json
+        rc = yajl.monkeypatch()
+        self.assertTrue(rc)
+        self.assertEquals(sys.modules['json'], sys.modules['yajl'])
+
+
 class IssueSixteenTest(unittest.TestCase):
     def runTest(self):
         dumpable = [11889582081]
--- yajl.c
+++ yajl.c
@@ -343,6 +343,24 @@ static PyObject *py_dump(PYARGS)
     return result;
 }
 
+static PyObject *py_monkeypatch(PYARGS)
+{
+    PyObject *sys = PyImport_ImportModule("sys");
+    PyObject *modules = PyObject_GetAttrString(sys, "modules");
+    PyObject *yajl = PyDict_GetItemString(modules, "yajl");
+
+    if (!yajl) {
+        return Py_False;
+    }
+
+    PyDict_SetItemString(modules, "json_old", PyDict_GetItemString(modules, "json"));
+    PyDict_SetItemString(modules, "json", yajl);
+
+    Py_XDECREF(sys);
+    Py_XDECREF(modules);
+    return Py_True;
+}
+
 static struct PyMethodDef yajl_methods[] = {
     {"dumps", (PyCFunctionWithKeywords)(py_dumps), METH_VARARGS | METH_KEYWORDS,
 "yajl.dumps(obj [, indent=None])\n\n\
@@ -373,6 +391,10 @@ selects the most compact representation.\n\
     /*
      {"iterload", (PyCFunction)(py_iterload), METH_VARARGS, NULL},
      */
+    {"monkeypatch", (PyCFunction)(py_monkeypatch), METH_NOARGS,
+"yajl.monkeypatch()\n\n\
+Monkey-patches the yajl module into sys.modules as \"json\"\n\
+"},
     {NULL}
 };
 
