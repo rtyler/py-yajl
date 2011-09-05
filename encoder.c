f3789ce044c38e6392d319a74fefd9b3138a266a
Iain Bullard <iain.bullard@gmail.com>
2011-09-05 22:46:31 +0100

    Fixed memory leak in encoder.c ProcessObject
    Unicode buffer was not being freed after use

--- encoder.c
+++ encoder.c
@@ -142,7 +142,9 @@ static yajl_gen_status ProcessObject(_YajlEncoder *self, PyObject *object)
             }
         }
         buffer[offset] = '\0';
-        return yajl_gen_raw_string(handle, (const char *)(buffer), (unsigned int)(offset));
+        status = yajl_gen_raw_string(handle, (const char *)(buffer), (unsigned int)(offset));
+        free(buffer);
+        return status;
     }
 #ifdef IS_PYTHON3
     if (PyBytes_Check(object)) {
